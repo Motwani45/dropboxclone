ddeae616e5d4112c8aeaeca4dfcd950ab8eb53e8
Jai Motwani <jmotwani@asite.com>
2023-04-06 11:16:57 +0530

    File size new issue generated

--- lib/core/extension/filesize.dart
+++ lib/core/extension/filesize.dart
@@ -3,7 +3,10 @@ import 'dart:math';
 
 extension FileSize on File{
   Future<String> getFileSize() async{
-    final bytes=await length();
+    int bytes=await length();
+    if(bytes==0){
+      bytes=1;
+    }
     const suffixes = ["b", "kb", "mb", "gb", "tb"];
     var i = (log(bytes) / log(1000)).floor();
     return ((bytes / pow(1000, i)).toStringAsFixed(1)) + suffixes[i];
